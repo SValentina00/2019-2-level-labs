47f96ef78d1fbfa06c484621278d45ad2089b749
SValentina00 <55386384+SValentina00@users.noreply.github.com>
2019-10-14 13:00:23 +0300

    Update main.py
    remake

--- lab_1/main.py
+++ lab_1/main.py
@@ -3,56 +3,47 @@ Labour work #1.1
 Count frequencies dictionary by the given arbitrary text
 """
 
+def calculate_frequences(text):
+    prohibited_marks = ['.', ',', '!', '?', ';', ':', '"', "'", '—', '-',
+                        '$', '%', '*', '@', '^', '&', '~', '*', '+', '=']
+    if isinstance(text, str):
+        text = text.lower()
+        for word in text:
+            if word.isdigit() or word in prohibited_marks:
+                text = text.replace(word, ' ')
+        splitted = (text.replace('\n', ' ')).split()
+        dictionary = {}
+        for key in splitted:
+            if key in dictionary:
+                value = dictionary[key]
+                dictionary[key] = value + 1
+            else:
+                dictionary[key] = 1
+        return dictionary
+    return {}
+def filter_stop_words(dictionary, stop_words):
+    if dictionary and stop_words is not None:
+        dict_clear = dictionary.copy()
+        for key in dictionary:
+            if not isinstance(key, str):
+                del dict_clear[key]
+        for key in list(dict_clear.keys()):
+            if key in stop_words:
+                del dict_clear[key]
+        return dict_clear
+    return {}
 
-def calculate_frequences(text: str):
-   prohibited_marks = ['-','.',',','!','?','"',"'", ':',';']
-    splitted = (text.replace('\n', ' ')).split(' ')
-    res = []
-    for word in splitted:
-        if not word.isdigit() and word not in prohibited_marks:
-            clear_word = ''
-            for el in word:
-                if el not in prohibited_marks:
-                    clear_word += el
-            if clear_word is not '':
-                res.append(clear_word)
-    d = {}
-    for key in res:
-        if key in d:
-            value = d[key]
-            d[key] = value+1
-        else:
-            d[key]=1
-    return d
-    pass
-
-def filter_stop_words(d: dict, stop_words: tuple):
-    for key in list(d.keys()):
-        if key in stop_words:
-            del d[key]
-    return d
-    pass
-
-def get_top_n(dict_clear: dict, top_n: int):
-    list_dict = list(dict.items())
-    list_dict.sort(key=lambda i: (i[1],i[0]), reverse=True)
+def get_top_n(dict_clear, top_n):
+    list_dict = list(dict_clear.items())
+    list_dict.sort(key=lambda x: -x[1])
     ind = 0
-    done = list()
-    for i in list_dict:
-        if ind <= top_n:
-            done.append(list_dict[ind])
-            ind += 1
-        else:
-            break
-    print (done)
-    pass
-start = open ('text.txt', 'r')
-text = start.read().lower()
-
-dict = calculate_frequences(text)
-stop_words = ('a','the','for','and', 'is', 'to',)
-print (filter_stop_words(dict, stop_words))
-
-dict_clear = filter_stop_words(dict, stop_words)
-top_n = 5
-get_top_n(dict_clear, top_n)  
+    res = [х[0] for х in list_dict]
+    top = list()
+    if top_n > 0:
+        
+        for i in res:
+            if ind < top_n:
+                top.append(res[ind])
+                ind += 1
+        done = tuple(top)
+        return done
